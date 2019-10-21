71b0b13857d724278f104105f2286f5071523517
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-21 18:06:52 +0200

    form Input and custom button component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 /* const HatsPage = () => (s
   <div>
     <h1>Hats Page</h1>
   </div>
-) */ function App() {
+) */
+
+function App() {
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
